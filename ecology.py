ee6cd38ced2e387d4779d6638deb4cdf5c67bbb6
nastts <159527618+nastts@users.noreply.github.com>
2024-03-23 12:34:40 +0300

    Add files via upload

---
+++ ecology.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands 
+import os
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+
+@bot.command()
+async def ecology_mem(ctx):
+    img_lst = os.listdir('images')
+    with open(f'images/{random.choice(img_lst)}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def helping_nature(ctx):
+    await ctx.send(f'привет!!! я очень рад, что ты хочешь миру стать лучше')
+    await ctx.send(f'самое важное: сортировка мусора. без этого ничего не получится')
+    pon = os.listdir('pon')
+    with open(f'pon/{random.choice(pon)}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+    await ctx.send(f'если каждый человек будет сортировать мусор, мир станет в несколько раз чище')
+bot.run('token')
